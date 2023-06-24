9ef412c53b57fe62588388e0bb9a5bf15a1de933
blaiselcq <blaiselcq@blaiselcq.net>
2023-06-24 18:53:42 +0200

    Add day template

---
+++ src/days/day_00.rs
@@ -0,0 +1,21 @@
+pub fn puzzle_1(input: &str) -> u32 {}
+
+pub fn puzzle_2(input: &str) -> u32 {}
+
+#[cfg(test)]
+mod tests {
+
+    fn get_input() -> String {
+        let day = 0;
+        let input_file_path = format!("./data/tests/test{:02}.txt", day);
+        std::fs::read_to_string(input_file_path).unwrap()
+    }
+
+    use super::*;
+
+    #[test]
+    fn test_puzzle_1() {}
+
+    #[test]
+    fn test_puzzle_2() {}
+}
