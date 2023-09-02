a9dd381d6d57a7485933000c27f8b70d2d9a0ea0
blaiselcq <blaiselcq@blaiselcq.net>
2023-09-02 13:38:11 +0200

    Day 11 - seoncd part

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -1,25 +1,22 @@
 mod types {
     use std::collections::VecDeque;
 
-    pub type Operation = dyn Fn(u32) -> Option<u32>;
+    pub type Operation = dyn Fn(u64) -> Option<u64>;
     pub struct Monkey {
-        pub items: VecDeque<u32>,
+        pub items: VecDeque<u64>,
         pub operation: Box<Operation>,
-        pub test_number: u32,
+        pub test_number: u64,
         pub recipient_success: usize,
         pub recipient_failed: usize,
     }
 
     impl Monkey {
-        pub fn play(&mut self, divide_worry: bool) -> Option<(usize, u32)> {
-            let value = self.items.pop_front();
-            value?;
-            let worry_level = (self.operation)(value.unwrap());
-            worry_level?;
+        pub fn play(&mut self, managed_worry: bool, divisor: u64) -> Option<(usize, u64)> {
+            let value = self.items.pop_front()?;
 
-            let worry_level = match divide_worry {
-                true => worry_level.unwrap() / 3,
-                false => worry_level.unwrap(),
+            let worry_level = match managed_worry {
+                false => (self.operation)(value % divisor)?,
+                true => (self.operation)(value)? / 3,
             };
 
             match worry_level % self.test_number == 0 {
@@ -28,13 +25,14 @@ mod types {
             }
         }
 
-        pub fn throw(&mut self, value: u32) {
+        pub fn throw(&mut self, value: u64) {
             self.items.push_back(value);
         }
     }
 }
 
 mod parser {
+    use core::panic;
     use std::num::ParseIntError;
 
     use nom::{
@@ -51,7 +49,7 @@ mod parser {
     #[derive(Debug, Clone, Copy)]
     enum Operand {
         Old,
-        Number(u32),
+        Number(u64),
     }
 
     fn map_operand(operand: &str) -> Result<Operand, ParseIntError> {
@@ -84,13 +82,13 @@ mod parser {
         let operand_2 = operand_2.unwrap();
 
         let operator = match operator_str {
-            '+' => u32::checked_add,
-            '-' => u32::checked_sub,
-            '*' => u32::checked_mul,
+            '+' => u64::checked_add,
+            '-' => panic!(),
+            '*' => u64::checked_mul,
             _ => panic!(),
         };
 
-        let operation: Box<dyn Fn(u32) -> Option<u32>> = match (operand_1, operand_2) {
+        let operation: Box<dyn Fn(u64) -> Option<u64>> = match (operand_1, operand_2) {
             (Operand::Old, Operand::Old) => Box::new(move |old| operator(old, old)),
             (Operand::Old, Operand::Number(n2)) => Box::new(move |old| operator(old, n2)),
             (Operand::Number(n1), Operand::Old) => Box::new(move |old| operator(n1, old)),
@@ -100,8 +98,8 @@ mod parser {
         Ok((input, operation))
     }
 
-    fn parse_test(input: &str) -> IResult<&str, (u32, usize, usize)> {
-        let (input, divisor) = preceded(tag("divisible by "), complete::u32)(input)?;
+    fn parse_test(input: &str) -> IResult<&str, (u64, usize, usize)> {
+        let (input, divisor) = preceded(tag("divisible by "), complete::u64)(input)?;
         let (input, _) = newline(input)?;
         let (input, _) = take_until(": ")(input)?;
         let (input, monkey_1) = preceded(tag(": throw to monkey "), complete::u8)(input)?;
@@ -113,11 +111,11 @@ mod parser {
     }
 
     fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
-        let (input, _) = delimited(tag("Monkey "), complete::u32, tag(":"))(input)?;
+        let (input, _) = delimited(tag("Monkey "), complete::u64, tag(":"))(input)?;
         let (input, _) = newline(input)?;
         let (input, items) = preceded(
             tuple((multispace1, tag("Starting items: "))),
-            separated_list0(tag(", "), complete::u32),
+            separated_list0(tag(", "), complete::u64),
         )(input)?;
         let (input, _) = newline(input)?;
         let (input, operation) =
@@ -161,16 +159,37 @@ mod parser {
     }
 }
 
-pub fn monkey_play(
+fn lcm(nums: &[u64]) -> u64 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
+}
+
+fn monkey_play(
     iterations: usize,
     mut monkeys: Vec<types::Monkey>,
-    divide_worry: bool,
+    managed_worry: bool,
 ) -> Vec<usize> {
     let mut pass = vec![0; monkeys.len()];
 
+    let divisor = match managed_worry {
+        true => 3,
+        false => lcm(&monkeys.iter().map(|m| m.test_number).collect::<Vec<_>>()),
+    };
+
     for _ in 0..iterations {
         for i in 0..monkeys.len() {
-            while let Some(play) = monkeys[i].play(divide_worry) {
+            while let Some(play) = monkeys[i].play(managed_worry, divisor) {
                 let (recipient, value) = play;
                 monkeys[recipient].throw(value);
                 pass[i] += 1;
@@ -191,7 +210,7 @@ pub fn puzzle_1(input: &str) -> String {
 
 pub fn puzzle_2(input: &str) -> String {
     let monkeys = parser::parse_input(input);
-    let mut pass = monkey_play(100, monkeys, false);
+    let mut pass = monkey_play(10000, monkeys, false);
 
     pass.sort();
 
